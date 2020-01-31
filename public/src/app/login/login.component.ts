e770e6914eaca990288a717a4aa8fea8a47fae00
zach <zbloch16@gmail.com>
2020-01-30 19:31:33 -0800

    login works but does not access user

--- public/src/app/login/login.component.ts
+++ public/src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 import { HttpService } from '../http.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-// import { AppComponent } from '../app.component';
+// import { EventEmitter } from 'protractor';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-login',
@@ -11,34 +12,41 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 
 export class LoginComponent implements OnInit {
   checkUser : any;
-  inSession: number;
+  // inSession: number;
+  @Output() aTaskEventEmitter = new EventEmitter();
+  
   constructor(
     // private _updateSession: AppComponent,
     private _httpService: HttpService,
     private _route: ActivatedRoute,
     private _router: Router
   ) { }
-
   ngOnInit() {
     this.checkUser = {email: "", password: ""};
-    this.inSession = 0;
   }
+    triggerEvent(){
+      //  2b. Emit the Event
+      this.aTaskEventEmitter.emit(false); //we can pass in any data type
+    }
   userLogin(checkUser){
-    let obs = this._httpService.loginUser(checkUser);
-    obs.subscribe((data:any) => {
-      console.log(data)
-      if(data.email){
-        console.log("logged in user!");
-        this.inSession = data.id;
-        // this._updateSession.getSession(this.inSession);
-        console.log(this.inSession);
-        // this._router.navigate(['']);
-        this._router.navigate(['/dashboard']);
-            }
-      else{
-        console.log("error");
-      }
-    })
+    this.triggerEvent();
+    this._router.navigate(['/dashboard']);
+
+    // let obs = this._httpService.loginUser(checkUser);
+    // obs.subscribe((data:any) => {
+    //   console.log(data)
+    //   if(data.email){
+    //     console.log("logged in user!");
+    //     // this.inSession = data.id;
+    //     // this._updateSession.getSession(this.inSession);
+    //     // console.log(this.inSession);
+    //     // this._router.navigate(['']);
+    //     this._router.navigate(['/dashboard']);
+    //         }
+    //   else{
+    //     console.log("error");
+    //   }
+    // })
   }
 
 }
